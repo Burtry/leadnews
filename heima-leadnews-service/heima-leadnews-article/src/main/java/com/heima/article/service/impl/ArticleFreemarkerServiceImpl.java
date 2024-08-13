56d36a78366b0895702dad4ee5254c07c01893ca
Burtry <1971797331@qq.com>
2024-08-13 17:12:38 +0800

    update code --- 新增文章同步创建索引

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -1,18 +1,23 @@
 package com.heima.article.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.heima.article.mapper.ApArticleContentMapper;
 import com.heima.article.service.ApArticleService;
 import com.heima.article.service.ArticleFreemarkerService;
+import com.heima.common.constants.ArticleConstants;
 import com.heima.file.service.FileStorageService;
 import com.heima.model.pojo.article.ApArticle;
 import com.heima.model.pojo.article.ApArticleContent;
+import com.heima.model.vo.search.SearchArticleVo;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,6 +45,9 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
     @Autowired
     private ApArticleService articleService;
 
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
     @Override
     @Async
     public void buildArticleToMinio(ApArticle apArticle, String content) {
@@ -59,16 +67,35 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-
-
             //将html上传到minio中
             InputStream byteArrayInputStream = new ByteArrayInputStream(stringWriter.toString().getBytes());
             String path = fileStorageService.uploadHtmlFile("", apArticle.getId() + ".html", byteArrayInputStream);
 
             //修改ap_article表，保存url字段
             articleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId,apArticle.getId()).set(ApArticle::getStaticUrl,path));
+
+            //发送消息，创建索引
+            createArticleESIndex(apArticle,content,path);
+
         }
 
+
+    }
+
+    /**
+     * 发送消息，创建索引
+     * @param apArticle
+     * @param content
+     * @param path
+     */
+    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
+
+        SearchArticleVo searchArticleVo = new SearchArticleVo();
+
+        BeanUtils.copyProperties(apArticle,searchArticleVo);
+        searchArticleVo.setContent(content);
+        searchArticleVo.setStaticUrl(path);
+        kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(searchArticleVo));
 
     }
 }
